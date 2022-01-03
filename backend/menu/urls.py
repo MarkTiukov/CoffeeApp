9c2878b3c460935d1053610179de3c1400310268
MarkTiukov <tyukov.mark@gmail.com>
2022-01-03 16:13:26 +0300

    downloading categories now

--- backend/menu/urls.py
+++ backend/menu/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from menu.views import MenuItemList
+from menu.views import MenuItemList, MenuCategoryList
 
 urlpatterns = [
-    # path('categories/', ),
+    path('categories/', MenuCategoryList.as_view()),
     path('items/', MenuItemList.as_view()),
 ]
--- coffee-app/src/actions/menu_actions.js
+++ coffee-app/src/actions/menu_actions.js
@@ -5,3 +5,9 @@ export const get_all_menu_items = () => {
         .then(data => data.json())
         .then(data => console.log(data))
 };
+
+export const get_all_categories = () => {
+    return fetch(baseURL + 'categories/')
+        .then(data => data.json())
+        .then(data => { return data });
+};
